355e43c331bc0a6211c25b98047d2e3c165af9d6
Eugene Kovshilovsky <4632199+ekovshilovsky@users.noreply.github.com>
2026-03-17 12:46:24 -0700

    Add cloister formula (v0.0.1)

---
+++ Formula/cloister.rb
@@ -0,0 +1,24 @@
+class Cloister < Formula
+  desc "Isolated environments for multiple Claude Code accounts"
+  homepage "https://github.com/ekovshilovsky/cloister"
+  version "0.0.1"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/ekovshilovsky/cloister/releases/download/v0.0.1/cloister_0.0.1_darwin_arm64.tar.gz"
+      sha256 "6d920d4faaf8a6ba6116d04ca87d91f63b88b1de855d51b564170e322922a6ef"
+    else
+      url "https://github.com/ekovshilovsky/cloister/releases/download/v0.0.1/cloister_0.0.1_darwin_amd64.tar.gz"
+      sha256 "e1cb89740aeae1f9e369b0f0d0db526bd530fc3a736886e45e63d2bd51315275"
+    end
+  end
+
+  def install
+    bin.install "cloister"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/cloister version")
+  end
+end
