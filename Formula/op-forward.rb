124a1e4e36990ff1a9c1e0a4fe406b3f7233aa1a
Eugene Kovshilovsky <4632199+ekovshilovsky@users.noreply.github.com>
2026-03-16 17:37:50 -0700

    Update op-forward to 0.3.0

--- Formula/op-forward.rb
+++ Formula/op-forward.rb
@@ -1,26 +1,26 @@
 class OpForward < Formula
   desc "Forward 1Password CLI across SSH boundaries with biometric auth"
   homepage "https://github.com/ekovshilovsky/op-forward"
-  version "0.2.0"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ekovshilovsky/op-forward/releases/download/v0.2.0/op-forward_0.2.0_darwin_arm64.tar.gz"
-      sha256 "3c617d9fa86dc0930c7fe9d91e4270de601ec1412e1994ef21b7888844344605"
+      url "https://github.com/ekovshilovsky/op-forward/releases/download/v0.3.0/op-forward_0.3.0_darwin_arm64.tar.gz"
+      sha256 "8bee8a5b94ff16115f6b085890b3364ada5d7a0745810f22d9747587073b47eb"
     else
-      url "https://github.com/ekovshilovsky/op-forward/releases/download/v0.2.0/op-forward_0.2.0_darwin_amd64.tar.gz"
-      sha256 "394f91867ff4aa69cd18cd4d4ea89283e504624aac032483f39eee1afe1bc379"
+      url "https://github.com/ekovshilovsky/op-forward/releases/download/v0.3.0/op-forward_0.3.0_darwin_amd64.tar.gz"
+      sha256 "23df3ed3b8555951ba7152290207d60a54aa9b646769da2c654fabf0523f916b"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/ekovshilovsky/op-forward/releases/download/v0.2.0/op-forward_0.2.0_linux_arm64.tar.gz"
-      sha256 "da338706eeae4a51583c78d63dcedc73de11fea3a88a8fa36be6843753a07a83"
+      url "https://github.com/ekovshilovsky/op-forward/releases/download/v0.3.0/op-forward_0.3.0_linux_arm64.tar.gz"
+      sha256 "60238db904c65596bb51fc41650c6ce428d42c2bf7f1cbdf7ff6eab3d66c9382"
     else
-      url "https://github.com/ekovshilovsky/op-forward/releases/download/v0.2.0/op-forward_0.2.0_linux_amd64.tar.gz"
-      sha256 "49afad0632533d12cf9b35bb03e455adffca39e24fb3baab89005b635f75da97"
+      url "https://github.com/ekovshilovsky/op-forward/releases/download/v0.3.0/op-forward_0.3.0_linux_amd64.tar.gz"
+      sha256 "ee07ccd3223b03750527f900bf05d770f7722c2f8937cd3be2881c146e3093e4"
     end
   end
 
